8f502bd1282736f2bd8cfd71f0b74825e8500556
rafaelgss <rafael.nunu@hotmail.com>
2018-04-23 22:33:36 -0300

    outhook ok

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # pylack
+
+https://bot-pylack.slack.com/services/352922744983?updated=1#service_setup
+
+
+```
+ImmutableMultiDict(
+[
+('token', '6aEIbL0AaoylQbD9HNSlKYDR'),
+('timestamp', '1524531966.000188'),
+('channel_name', 'general'),
+('trigger_word', 'paulinho'),
+('team_id', 'TAC09TX6K'),
+('user_id', 'UAB2X1Z7F'),
+('user_name', 'rafael.nunu'),
+('text', 'paulinho: que isso ?'),
+('service_id', '352922744983'),
+('channel_id', 'CAB2VSA7P'),
+('team_domain', 'bot-pylack')
+
+ ])
+ 
+```
--- routes/web.py
+++ routes/web.py
@@ -1,6 +1,31 @@
 from bootstrap.main_app import app
+from flask import request
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return 'Hello worlds!'
+    data = request.form
+    with open("test.txt", "w") as files:
+        files.write(str(data))
+    return "Hello moto"
+
+
+@app.route('/api/outhook', methods=['POST', 'GET'])
+def outhook():
+    data = request.form
+    with open("test.txt", "w") as files:
+        files.write(str(data))
+
+    #POST MESSAGE SERVICE SLACK AND STORE IN DATABASE
+    return "Hello moto"
+
+
+@app.route('/api/private/humor')
+# Receiving data and store a database for analytics
+def outhook_humor():
+    pass
+
+
+def check_token():
+    # VERIFY IN ENV
+    pass
